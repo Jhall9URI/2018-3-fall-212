7812aaa33b3efedf76aeef6231fc2464d88734ed
eben0012 <32018897+eben0012@users.noreply.github.com>
2018-11-28 08:47:03 -0500

    lab 9 uploaded

---
+++ lab-09/bst.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#include "bst.hpp"
+
+BSTree::BSTree() {
+    // TODO: Implement constructor
+}
+
+BSTree::~BSTree() {
+    // TODO: Implement destructor
+}
+
+void BSTree::clear() {
+    // TODO: Implement public clear function
+    return;
+}
+
+void BSTree::clear(BSTNode *node) {
+    // TODO: Implement private recursive clear function
+    return;
+}
+
+void BSTree::insert(int val, char let) {
+    // TODO: Implement public insert function
+    return;
+}
+
+void BSTree::insert(int val, char let, BSTNode *node) {
+    // TODO: Implement private recursive insert function
+    return;
+}
+
+bool BSTree::search(int val) {
+    // TODO: Implement public search function
+    return false;
+}
+
+bool BSTree::search(int val, BSTNode *node) {
+    // TODO: Implement private recursive search function
+    return false;
+}
